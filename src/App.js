48dbd146036abf16f362ada872bcb6e8599c0d4a
Oleg Parshyn <olegparshyn@MacBook-Pro-LoopMe-3.local>
2019-11-15 09:30:56 +0200

    Posts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Post } from './Components/Post.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            posts: []
+        };
+    }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => this.setState({ posts: json }));
+    }
+    render() {
+        return this.state.posts.map((post, index) => (
+            <Post key={index} props={post} />
+        ));
+    }
 }
 
 export default App;
